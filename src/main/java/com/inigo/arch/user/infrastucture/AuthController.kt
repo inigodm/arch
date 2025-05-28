c015602fc400292c8511fae34c352d5eb97216dd
inigodm <inigo.delgado@gmail.com>
2025-05-28 09:59:47 +0200

    Spring validation for params

--- src/main/java/com/inigo/arch/user/infrastucture/AuthController.kt
+++ src/main/java/com/inigo/arch/user/infrastucture/AuthController.kt
@@ -3,9 +3,10 @@ package com.inigo.arch.user.infrastucture
 import com.inigo.arch.user.application.Login
 import com.inigo.arch.user.domain.Password
 import com.inigo.arch.user.domain.Username
-import lombok.AllArgsConstructor
-import lombok.Data
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,19 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/auth")
+@Validated
 class AuthController(val login: Login) {
     @PostMapping("/login")
-    fun login(@RequestBody request: AuthRequest): ResponseEntity<*> {
+    fun login(@Valid @RequestBody request: AuthRequest): ResponseEntity<*> {
         val token = login.execute(Username(request.username!!), Password(request.password!!))
         return ResponseEntity.ok(AuthResponse(token.value))
     }
 }
 
-@Data
 class AuthRequest {
-    val username: String? = null
-    val password: String? = null
+    @field:NotNull(message = "username must not be null") val username: String? = null
+    @field:NotNull(message = "password must not be null") val password: String? = null
 }
-@Data
-@AllArgsConstructor
+
 data class AuthResponse (val token: String)
